23e7d5fb864685d86e4a0a5c9189132c97640597
Keckich <nik.malashko@mail.ru>
2025-08-20 13:30:43 +0200

    implemented Updated and Deleted consumers for search service

---
+++ SearchService/Consumers/AuctionDeletedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class AuctionDeletedConsumer : IConsumer<AuctionDeleted>
+    {
+        private readonly IMapper _mapper;
+
+        public AuctionDeletedConsumer(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionDeleted> context)
+        {
+            var id = context.Message.Id;
+            Console.WriteLine("--> Consuming auction updated: " + id);
+
+            var result = await DB.DeleteAsync<Item>(id);
+
+            if (!result.IsAcknowledged)
+            {
+                throw new MessageException(typeof(AuctionUpdated), "Problem deleting auciton");
+            }
+        }
+    }
+}
